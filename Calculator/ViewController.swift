697da0282b9eb72fab146c21de3b51493d945b82
m2m tech <tech@m2m.at>
2015-01-26 18:21:51 +0100

    end of lecture #2

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
 
     @IBAction func appendDigit(sender: UIButton)
     {
@@ -27,6 +27,58 @@ class ViewController: UIViewController
         }
         
         
+    }
+    
+    @IBAction func operate(sender: UIButton)
+    {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            case "×": performOperation { $0 * $1 }
+            case "÷": performOperation { $1 / $0 }
+            case "+": performOperation { $0 + $1 }
+            case "−": performOperation { $1 - $0 }
+            case "√": performOperation { sqrt($0) }
+            default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double)
+    {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+
+    func performOperation(operation: Double -> Double)
+    {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter()
+    {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
+    
+    var displayValue: Double
+    {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Calculator demon of the cs193p lecture (Winter 2015)
 
-written in Xcode 6.1.1 for iOS8.0+
+written in Xcode 6.1.1 for iOS8.1+
 
 
 ### various versions are available via branches and tags:
 
-[End of Lecture #1](http://cs193p.m2m.at/cs193p-lecture-1-logistics-ios8-overview-winter-2015/) -> [EndOfLecture2](https://github.com/m2mtech/calculator-2015/tree/EndOfLecture1)
+[End of Lecture #2](http://cs193p.m2m.at/cs193p-lecture-2-more-xcode-and-swift-mvc-winter-2015/) -> [EndOfLecture2](https://github.com/m2mtech/calculator-2015/tree/EndOfLecture2)
+[End of Lecture #1](http://cs193p.m2m.at/cs193p-lecture-1-logistics-ios8-overview-winter-2015/) -> [EndOfLecture1](https://github.com/m2mtech/calculator-2015/tree/EndOfLecture1)
