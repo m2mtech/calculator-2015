fc97e5736375626c4eafabe2bbbde354254c04e0
m2m tech <tech@m2m.at>
2015-02-14 23:32:41 +0100

    project #2 assignment #2 task #7

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -84,6 +84,50 @@ class CalculatorBrain
         }
     }
     
+    var description: String {
+        get {
+            var (result, ops) = ("", opStack)
+            do {
+                var current: String?
+                (current, ops) = description(ops)
+                result = result == "" ? current! : "\(current!), \(result)"
+            } while ops.count > 0
+            return result
+        }
+    }
+
+    private func description(ops: [Op]) -> (result: String?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (String(format: "%g", operand) , remainingOps)
+            case .NullaryOperation(let symbol, _):
+                return (symbol, remainingOps);
+            case .UnaryOperation(let symbol, _):
+                let operandEvaluation = description(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return ("\(symbol)(\(operand))", operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(let symbol, _):
+                let op1Evaluation = description(remainingOps)
+                if var operand1 = op1Evaluation.result {
+                    if remainingOps.count - op1Evaluation.remainingOps.count > 2 {
+                        operand1 = "(\(operand1))"
+                    }
+                    let op2Evaluation = description(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return ("\(operand2) \(symbol) \(operand1)", op2Evaluation.remainingOps)
+                    }
+                }
+            case .Variable(let symbol):
+                return (symbol, remainingOps)
+            }
+        }
+        return ("?", ops)
+    }
+    
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         if !ops.isEmpty {
             var remainingOps = ops
--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -20,4 +20,98 @@ class CalculatorTests: XCTestCase {
         XCTAssertEqual(6.28, brain.performOperation("+")!)
         //println("\(brain.program)")
     }
+
+    func testDescription() {
+        // cos(10)
+        var brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(10)!, 10)
+        XCTAssertTrue(brain.performOperation("cos")! - -0.839 < 0.1)
+        XCTAssertEqual(brain.description, "cos(10)")
+        
+        // 3 - 5
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertEqual(brain.performOperation("−")!, -2)
+        XCTAssertEqual(brain.description, "3 − 5")
+
+        // 23.5
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(23.5)!, 23.5)
+        XCTAssertEqual(brain.description, "23.5")
+
+        // π
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.performOperation("π")!, M_PI)
+        XCTAssertEqual(brain.description, "π")
+
+        // x
+        brain = CalculatorBrain()
+        XCTAssertNil(brain.pushOperand("x"))
+        XCTAssertEqual(brain.description, "x")
+        
+        // √(10) + 3
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(10)!, 10)
+        XCTAssertTrue(brain.performOperation("√")! - 3.162 < 0.1)
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertTrue(brain.performOperation("+")! - 6.162 < 0.1)
+        XCTAssertEqual(brain.description, "√(10) + 3")
+        
+        // √(3 + 5)
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertEqual(brain.performOperation("+")!, 8)
+        XCTAssertTrue(brain.performOperation("√")! - 2.828 < 0.1)
+        XCTAssertEqual(brain.description, "√(3 + 5)")
+        
+        // 3 + (5 + 4)
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertEqual(brain.pushOperand(4)!, 4)
+        XCTAssertEqual(brain.performOperation("+")!, 9)
+        XCTAssertEqual(brain.performOperation("+")!, 12)
+        XCTAssertEqual(brain.description, "3 + (5 + 4)")
+        
+        // √(3 + √(5)) ÷ 6
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertTrue(brain.performOperation("√")! - 2.236 < 0.1)
+        XCTAssertTrue(brain.performOperation("+")! - 5.236 < 0.1)
+        XCTAssertTrue(brain.performOperation("√")! - 2.288 < 0.1)
+        XCTAssertEqual(brain.pushOperand(6)!, 6)
+        XCTAssertTrue(brain.performOperation("÷")! - 0.381 < 0.1)
+        XCTAssertEqual(brain.description, "√(3 + √(5)) ÷ 6")
+        
+        // ? + 3
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertNil(brain.performOperation("+"))
+        XCTAssertEqual(brain.description, "? + 3")
+        
+        // √(3 + 5), cos(π)
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertEqual(brain.performOperation("+")!, 8)
+        XCTAssertTrue(brain.performOperation("√")! - 2.828 < 0.1)
+        XCTAssertEqual(brain.performOperation("π")!, M_PI)
+        XCTAssertEqual(brain.performOperation("cos")!, -1)
+        XCTAssertEqual(brain.description, "√(3 + 5), cos(π)")
+        
+        // 3 * (5 + 4)
+        brain = CalculatorBrain()
+        XCTAssertEqual(brain.pushOperand(3)!, 3)
+        XCTAssertEqual(brain.pushOperand(5)!, 5)
+        XCTAssertEqual(brain.pushOperand(4)!, 4)
+        XCTAssertEqual(brain.performOperation("+")!, 9)
+        XCTAssertEqual(brain.performOperation("×")!, 27)
+        XCTAssertEqual(brain.description, "3 × (5 + 4)")
+
+//        println("\(brain.program)")
+    }
+
 }
